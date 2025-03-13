53ca08f3c99e8eff3a1785c646e312b0e7468d18
FTXPart2 <wesleychung98@gmail.com>
2025-03-13 09:31:09 -0700

    new stuff

--- Runner.java
+++ Runner.java
@@ -1,6 +1,12 @@
 import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Scanner;
 
-public class Runner{
+public class Runner {
 
     public static void main(String[] args) {
         Graph<Location> graph = new Graph<>();
@@ -8,22 +14,135 @@ public class Runner{
         Location berlin = new Location("Berlin", "BER", 800, 400);
         Location munich = new Location("Munich", "MUC", 900, 700);
         Location hamburg = new Location("Hamburg", "HAM", 700, 200);
+        Location nuremburg = new Location("Nuremburg", "NUM", 850, 600);
+        Location stuttgart = new Location("Stuttgart", "STU", 800, 650);
+        Location frankfurt = new Location("Frankfurt", "FRA", 750, 550);
+        Location dusseldorf = new Location("Dusseldorf", "DUS", 650, 500);
+        Location cologne = new Location("Cologne", "COL", 640, 520);
+        Location dresden = new Location("Dresden", "DRS", 900, 450);
+        Location leipzig = new Location("Leipzig", "LEI", 850, 450);
+        Location hannover = new Location("Hannover", "HAN", 750, 350);
+        Location bremen = new Location("Bremen", "BRE", 700, 300);
+        Location dortmund = new Location("Dortmund", "DOR", 670, 480);
+        Location essen = new Location("Essen", "ESS", 660, 490);
+        Location duisburg = new Location("Duisburg", "DUI", 655, 495);
+        Location bochum = new Location("Bochum", "BOC", 665, 485);
+        Location wuppertal = new Location("Wuppertal", "WUP", 660, 510);
+        Location bielefeld = new Location("Bielefeld", "BIE", 700, 450);
+        Location bonn = new Location("Bonn", "BON", 640, 530);
+        Location munster = new Location("Munster", "MUN", 680, 460);
         // Add more locations here...
 
         graph.addVertex(berlin);
         graph.addVertex(munich);
         graph.addVertex(hamburg);
+        graph.addVertex(nuremburg);
+        graph.addVertex(stuttgart);
+        graph.addVertex(frankfurt);
+        graph.addVertex(dusseldorf);
+        graph.addVertex(cologne);
+        graph.addVertex(dresden);
+        graph.addVertex(leipzig);
+        graph.addVertex(hannover);
+        graph.addVertex(bremen);
+        graph.addVertex(dortmund);
+        graph.addVertex(essen);
+        graph.addVertex(duisburg);
+        graph.addVertex(bochum);
+        graph.addVertex(wuppertal);
+        graph.addVertex(bielefeld);
+        graph.addVertex(bonn);
+        graph.addVertex(munster);
+
         // Add more vertices here...
 
-        graph.addEdge(berlin, munich, 500);
-        graph.addEdge(berlin, hamburg, 300);
-        graph.addEdge(munich, hamburg, 600);
+        graph.addEdge(hamburg, bremen, 60);
+        graph.addEdge(bremen, hannover, 100);
+        graph.addEdge(bremen, bielefeld, 150);
+        graph.addEdge(hannover, berlin, 200);
+        graph.addEdge(hannover, leipzig, 200);
+        graph.addEdge(berlin, leipzig, 150);
+        graph.addEdge(leipzig, dresden, 100);
+        graph.addEdge(leipzig, nuremburg, 200);
+        graph.addEdge(nuremburg, munich, 150);
+        graph.addEdge(nuremburg, stuttgart, 200);
+        graph.addEdge(stuttgart, munich, 100);
+        graph.addEdge(stuttgart, frankfurt, 100);
+        graph.addEdge(nuremburg, frankfurt, 150);
+        graph.addEdge(frankfurt, bonn, 100);
+        graph.addEdge(bonn, cologne, 20);
+        graph.addEdge(cologne, dusseldorf, 20);
+        graph.addEdge(bonn, wuppertal, 50);
+        graph.addEdge(cologne, dusseldorf, 20);
+        graph.addEdge(dusseldorf, bochum, 10);
+        graph.addEdge(dusseldorf, essen, 10);
+        graph.addEdge(essen, duisburg, 10);
+        graph.addEdge(essen, dortmund, 20);
+        graph.addEdge(dortmund, munster, 20);
+        graph.addEdge(munster, bielefeld, 200);
+        
         // Add more edges here...
 
         JFrame frame = new JFrame("Graph Display");
+        Screen screen = new Screen(graph);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920, 1080);
-        frame.add(new Screen(graph));
+        frame.setLayout(new BorderLayout());
+        frame.add(screen, BorderLayout.CENTER);
+
+        JPanel controlPanel = new JPanel();
+        JTextField startField = new JTextField(5);
+        JTextField endField = new JTextField(5);
+        JButton findPathButton = new JButton("Find Path");
+        JTextArea directionsArea = new JTextArea(10, 30);
+        directionsArea.setEditable(false);
+        controlPanel.add(new JLabel("Start:"));
+        controlPanel.add(startField);
+        controlPanel.add(new JLabel("End:"));
+        controlPanel.add(endField);
+        controlPanel.add(findPathButton);
+        controlPanel.add(new JScrollPane(directionsArea));
+        frame.add(controlPanel, BorderLayout.SOUTH);
+
+        findPathButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String startAbbv = startField.getText().toUpperCase();
+                String endAbbv = endField.getText().toUpperCase();
+
+                Location start = null, end = null;
+                for (Location loc : graph.getVertices()) {
+                    if (loc.getAbbv().equals(startAbbv)) {
+                        start = loc;
+                    }
+                    if (loc.getAbbv().equals(endAbbv)) {
+                        end = loc;
+                    }
+                }
+
+                if (start != null && end != null) {
+                    List<Location> path = graph.getPath(start, end);
+                    int totalDistance = 0;
+                    StringBuilder directions = new StringBuilder("Directions:\n");
+                    for (int i = 0; i < path.size() - 1; i++) {
+                        Location from = path.get(i);
+                        Location to = path.get(i + 1);
+                        int distance = graph.getAdjWeight(from, to);
+                        totalDistance += distance;
+                        String roadKey = from.getAbbv() + "-" + to.getAbbv();
+                        String roadName = screen.getRoadName(roadKey); // Get road name from Screen
+                        directions.append("Take ").append(from.getName()).append(" to ").append(to.getName())
+                                  .append(" via ").append(roadName).append(" - ").append(distance).append(" miles.\n");
+                    }
+                    directions.append("The total distance is ").append(totalDistance).append(" miles.");
+                    directionsArea.setText(directions.toString());
+                    screen.setPath(path, totalDistance);
+                } else {
+                    directionsArea.setText("Invalid start or end location.");
+                }
+            }
+        });
+
         frame.pack();
         frame.setVisible(true);
     }
--- Screen.java
+++ Screen.java
@@ -6,11 +6,15 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class Screen extends JPanel {
     private BufferedImage image;
     private Graph<Location> graph;
     private Map<Location, Point> positions;
+    private List<Location> path;
+    private int totalDistance;
+    private Map<String, String> roadNames; // New map to store road names
 
     public Screen(Graph<Location> graph) {
         this.graph = graph;
@@ -22,14 +26,58 @@ public class Screen extends JPanel {
         }
         setPreferredSize(new Dimension(1920, 1080));
         initializePositions();
+        this.path = null;
+        this.totalDistance = 0;
+        this.roadNames = new HashMap<>();
+        initializeRoadNames();
+    }
+
+    public void setPath(List<Location> path, int totalDistance) {
+        this.path = path;
+        this.totalDistance = totalDistance;
+        repaint();
     }
 
     private void initializePositions() {
         // Example positions for locations (you can customize this)
-        positions.put(new Location("Berlin", "BER", 300, 200), new Point(300, 200));
-        positions.put(new Location("Munich", "MUC", 200, 400), new Point(200, 400));
-        positions.put(new Location("Hamburg", "HAM", 150, 125), new Point(150, 125));
+        positions.put(new Location("Berlin", "BER", 300, 190), new Point(300, 200));
+        positions.put(new Location("Munich", "MUC", 200, 470), new Point(200, 470));
+        positions.put(new Location("Hamburg", "HAM", 180, 125), new Point(180, 125));
+        positions.put(new Location("Nuremburg", "NUM", 200, 375), new Point(200, 375));
+        positions.put(new Location("Stuttgart", "STU", 130, 450), new Point(130, 450));
+        positions.put(new Location("Frankfurt", "FRA", 110, 350), new Point(110, 350));
+        positions.put(new Location("Dusseldorf", "DUS", 60, 275), new Point(60, 275));
+        positions.put(new Location("Cologne", "COL", 80, 285), new Point(80, 285));
+        positions.put(new Location("Dresden", "DRS", 315, 290), new Point(315, 290));
+        positions.put(new Location("Leipzig", "LEI", 280, 275), new Point(280, 275));
+        positions.put(new Location("Hannover", "HAN", 200, 185), new Point(200, 185));
+        positions.put(new Location("Bremen", "BRE", 170, 155), new Point(170, 155));
+        positions.put(new Location("Dortmund", "DOR", 90, 260), new Point(90, 260));
+        positions.put(new Location("Essen", "ESS", 65, 240), new Point(65, 240));
+        positions.put(new Location("Duisburg", "DUI", 35, 260), new Point(35, 260));
+        positions.put(new Location("Bochum", "BOC", 30, 280), new Point(30, 280));
+        positions.put(new Location("Wuppertal", "WUP", 45, 300), new Point(45, 300));
+        positions.put(new Location("Bielefeld", "BIE", 170, 200), new Point(170, 200));
+        positions.put(new Location("Bonn", "BON", 100, 300), new Point(100, 300));
+        positions.put(new Location("Munster", "MUN", 105, 245), new Point(105, 245));
         // Add more locations here...
+    }
+
+    private void initializeRoadNames() {
+        // Example road names (you can customize this)
+        roadNames.put("BER-MUC", "Highway 101");
+        roadNames.put("MUC-BER", "Highway 101");
+        roadNames.put("BER-HAM", "Highway 102");
+        roadNames.put("HAM-BER", "Highway 102");
+        roadNames.put("MUC-HAM", "Highway 103");
+        roadNames.put("HAM-MUC", "Highway 103");
+        roadNames.put("HAM-NUM", "Highway 104");
+        roadNames.put("NUM-HAM", "Highway 104");
+        // Add more road names here...
+    }
+
+    public String getRoadName(String roadKey) {
+        return roadNames.getOrDefault(roadKey, "");
     }
 
     @Override
@@ -39,6 +87,9 @@ public class Screen extends JPanel {
             g.drawImage(image, 0, 0, this);
         }
         drawGraph(g);
+        if (path != null) {
+            drawPath(g);
+        }
     }
 
     private void drawGraph(Graphics g) {
@@ -62,35 +113,32 @@ public class Screen extends JPanel {
                         int midX = (p1.x + p2.x) / 2;
                         int midY = (p1.y + p2.y) / 2;
                         g.drawString(String.valueOf(graph.getAdjWeight(location, neighbor)), midX, midY);
+                        String roadKey = location.getAbbv() + "-" + neighbor.getAbbv();
+                        String roadName = roadNames.getOrDefault(roadKey, "");
+                        g.drawString(roadName, midX, midY + 15); // Display road name
                     }
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-        Graph<Location> graph = new Graph<>();
-        // Add locations and edges to the graph
-        Location berlin = new Location("Berlin", "BER", 800, 400);
-        Location munich = new Location("Munich", "MUC", 900, 700);
-        Location hamburg = new Location("Hamburg", "HAM", 700, 200);
-        // Add more locations here...
-
-        graph.addVertex(berlin);
-        graph.addVertex(munich);
-        graph.addVertex(hamburg);
-        // Add more vertices here...
-
-        graph.addEdge(berlin, munich, 500);
-        graph.addEdge(berlin, hamburg, 300);
-        graph.addEdge(munich, hamburg, 600);
-        // Add more edges here...
-
-        JFrame frame = new JFrame("Graph Display");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1920, 1080);
-        frame.add(new Screen(graph));
-        frame.pack();
-        frame.setVisible(true);
+    private void drawPath(Graphics g) {
+        g.setColor(Color.RED);
+        for (int i = 0; i < path.size() - 1; i++) {
+            Location start = path.get(i);
+            Location end = path.get(i + 1);
+            Point p1 = positions.get(start);
+            Point p2 = positions.get(end);
+            if (p1 != null && p2 != null) {
+                g.drawLine(p1.x, p1.y, p2.x, p2.y);
+                int midX = (p1.x + p2.x) / 2;
+                int midY = (p1.y + p2.y) / 2;
+                String roadKey = start.getAbbv() + "-" + end.getAbbv();
+                String roadName = roadNames.getOrDefault(roadKey, "Unknown Road");
+                g.drawString(roadName, midX, midY -15); // Display road name
+            }
+        }
     }
 }
+
+
